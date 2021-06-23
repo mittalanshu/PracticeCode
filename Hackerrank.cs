daa3e89f25768eeae0a202aabf641c85d49cb582
Anshu Agarwal <41819307+mittalanshu@users.noreply.github.com>
2021-06-23 18:38:08 +0530

    HACKERRANK IN STRING

---
+++ Hackerrank.cs
@@ -0,0 +1,60 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string hackerrankInString(string s)
+    {
+        
+        int res=0;
+        string hacker="hackerrank";
+         for(int j = 0; j < s.Length; j++){
+            if(res < hacker.Length && j < s.Length && hacker[res] == s[j])res++;
+        }
+
+        if(res == hacker.Length) return "YES";
+        else return "NO";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = Result.hackerrankInString(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
